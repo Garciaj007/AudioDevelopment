5b044aecf6846258633f43a015ae6e0e9e15af28
Garciaj007 <jurielgarcia2010@gmail.com>
2019-02-27 20:28:45 -0500

    Juce Audio Plugin Integration

---
+++ Assets/Scripts/PluginEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class PluginEditor : MonoBehaviour {
+
+    public AudioMixer mixer;
+
+    public void Start()
+    {
+        mixer.SetFloat("Gain", 0.0f);
+    }
+
+    public void SliderChanged(Slider s)
+    {
+        mixer.SetFloat("Gain", Mathf.Pow(10, (s.value / 20.0f)));
+    }
+     
+}
